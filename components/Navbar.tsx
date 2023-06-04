c7e1881e003d43d7efd757c99135a4de1d982198
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-06-04 18:40:38 +0200

    Added main page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,16 +6,16 @@ export default function Navbar() {
         <nav
             style={{
                 display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center",
-                borderBottom: "1px solid grey", height: "75px", padding : "0 20px", marginBottom: "1rem"
+                borderBottom: "1px solid grey", height: "75px", padding : "0 20px", marginBottom: "1rem",
             }}
         >
             <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "2rem"}}>
                 <h1>BLeader</h1>
-                <div style={{ display: "flex", flexDirection: "row", gap: "1rem", alignItems: "center" }}>
-                    <Link href="/Maps">Maps</Link>
-                    <Link href="/Rankings">Rankings</Link>
-                    <Link href="/Events">Events</Link>
-                    <Link href="/Clans">Clans</Link>
+                <div style={{ display: "flex", flexDirection: "row", gap: "1rem", alignItems: "center", textDecoration: "none" }}>
+                    <Link href="/Maps" style={{ textDecoration: "none" }} >Maps</Link>
+                    <Link href="/Rankings" style={{ textDecoration: "none" }}>Rankings</Link>
+                    <Link href="/Events" style={{ textDecoration: "none" }}>Events</Link>
+                    <Link href="/Clans" style={{ textDecoration: "none" }}>Clans</Link>
                 </div>
             </div>
             <div>
